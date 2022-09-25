bc2d3e583b8215ef91c23e31272a312776889939
Tran Trung Tho <tho.trantrung@shopee.com>
2022-09-26 01:27:17 +0700

    clean code

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io"
 
-	"github.com/mergermarket/go-pkcs7"
 	openssl "github.com/Luzifer/go-openssl/v4"
 )
 
@@ -41,39 +35,13 @@ const secretKey = "Sqrb[1R.1#.a~Kl5sdTM|6Z'65zhBi}~"
 func Encrypt(data string, secretKey string) (string, error) {
 	o := openssl.New()
 
-	enc, err := o.EncryptBytes(secretKey,[]byte(data),openssl.BytesToKeyMD5)
-	if err != nil {
-	  return "",err
-	}
-  
-return string(enc),nil
-
-	key := []byte(secretKey)
-	plainText := []byte(data)
-	plainText, err = pkcs7.Pad(plainText, aes.BlockSize)
-	if err != nil {
-		return "", fmt.Errorf(`plainText: "%s" has error`, plainText)
-	}
-	if len(plainText)%aes.BlockSize != 0 {
-		err := fmt.Errorf(`plainText: "%s" has the wrong block size`, plainText)
-		return "", err
-	}
-
-	block, err := aes.NewCipher(key)
+	enc, err := o.EncryptBytes(secretKey, []byte(data), openssl.BytesToKeyMD5)
 	if err != nil {
 		return "", err
 	}
 
-	cipherText := make([]byte, aes.BlockSize+len(plainText))
-	iv := cipherText[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return "", err
-	}
+	return string(enc), nil
 
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
-
-	return fmt.Sprintf("%x", cipherText), nil
 }
 
 // Decrypt decrypts cipher text string into plain text string
@@ -82,31 +50,9 @@ func Decrypt(encrypted string, secretKey string) (string, error) {
 
 	dec, err := o.DecryptBytes(secretKey, []byte(encrypted), openssl.BytesToKeyMD5)
 	if err != nil {
-		return "",err
-	}
- 
-	return string(dec),nil
-
-	key := []byte(secretKey)
-	cipherText, _ := hex.DecodeString(encrypted)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err)
+		return "", err
 	}
 
-	if len(cipherText) < aes.BlockSize {
-		panic("cipherText too short")
-	}
-	iv := cipherText[:aes.BlockSize]
-	cipherText = cipherText[aes.BlockSize:]
-	if len(cipherText)%aes.BlockSize != 0 {
-		panic("cipherText is not a multiple of the block size")
-	}
+	return string(dec), nil
 
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(cipherText, cipherText)
-
-	cipherText, _ = pkcs7.Unpad(cipherText, aes.BlockSize)
-	return fmt.Sprintf("%s", cipherText), nil
 }
